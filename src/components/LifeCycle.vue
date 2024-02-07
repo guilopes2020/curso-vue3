319a3e520426a6c87fc7a5b7b26e070fb32c982e
guilopes2020 <guilopesdev@gmail.com>
2024-02-07 12:08:54 -0300

    feat: :sparkles: make-life-cycle-hooks

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>meu nome é: {{ nome }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: 'ainda nao sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Guillherme'
+            }, 2000)
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'Pedro'
+            }, 4000)
+        }
+    }
+</script>
