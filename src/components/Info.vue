0b5abe4221d7991341d25808b89d20ef3d71999d
guilopes2020 <guilopesdev@gmail.com>
2024-02-07 19:56:49 -0300

    feat: :sparkles: make-component-hierarchy

---
+++ src/components/Info.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento</p>
+        <p>ultilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>Javascript</li>
+            <li>PHP</li>
+            <li>Phyton</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Info',
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
---
+++ src/components/Pessoa.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>esta é a descrição da pessoa {{ nome }}</h2>
+        <Info />
+        <Form />
+    </div>
+</template>
+
+<script>
+import Info from './Info'
+import Form from './Form.vue';
+    export default {
+        name: 'Pessoa',
+        data() {
+            return {
+                nome: 'Ana',
+                description: 'alta e morena'
+            }
+        },
+        components: {
+            Info,
+            Form
+        }
+    }
+</script>
