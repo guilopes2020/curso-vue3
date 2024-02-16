6d34c6f9d98234130d1ca3de5642ccab54873ce2
guilopes2020 <guilopesdev@gmail.com>
2024-02-16 14:10:24 -0300

    feat: :sparkles: make-events

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,12 +2,17 @@
     <div>
         <h1>Olá Vue!</h1>
         <p>meu nome é: {{ nome }}, tenho {{ age }} anos de idade e sou {{ profissao }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue';
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: 'Matheus',
