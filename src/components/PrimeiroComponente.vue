ecfd41941557febf27f5bb127c59fba46442dcfe
guilopes2020 <guilopesdev@gmail.com>
2024-02-19 08:06:16 -0300

    feat: :sparkles: reuse-of-components

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
+        <Reultilizacao />
         <h1>Olá Vue!</h1>
+        <Reultilizacao />
         <p>meu nome é: {{ nome }}, tenho {{ age }} anos de idade e sou {{ profissao }}</p>
+        <Reultilizacao />
         <MultiplosEventos />
+        <Reultilizacao />
+        <Reultilizacao />
+        <Reultilizacao />
+        <Reultilizacao />
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue';
+import Reultilizacao from './Reultilizacao.vue';
     export default {
         name: 'PrimeiroComponente',
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+            Reultilizacao
         },
         data() {
             return {
---
+++ src/components/Reultilizacao.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button @click="ativar">clique aqui</button>
+        <span v-show="ativou">Ativado!</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Reultilizacao',
+        data() {
+            return {
+                ativou: false
+            }
+        },
+        methods: {
+            ativar() {
+                this.ativou = !this.ativou
+            }
+        }
+    }
+</script>
